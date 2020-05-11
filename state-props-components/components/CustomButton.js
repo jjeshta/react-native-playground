7007088742976023f3c0216ea7507b6f1afc4fce
Jeshta Bhoyedhur <jeshta@lsl.digital>
2020-05-11 18:31:53 +0400

    updates

--- state-props-components/components/CustomButton.js
+++ state-props-components/components/CustomButton.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
-import {
-  View,
-  Text, 
-  Button
-} from 'react-native';
+import { View, Text, Button } from "react-native";
 
 const CustomButton = (props) => {
-    return (
-        <Button title="Add Candy" onPress= { ()=> props.addMyCandy()}/>
-    );
-}
+  return <Button title="Add Candy" onPress={props.addMyCandy()} />;
+};
 
 export default CustomButton;
